12d16cab05b8759aad752b14853042036e01e64b
Sanjay Patel <sapatel@academyart.edu>
2021-12-21 15:17:46 -0800

    Added copytodos function for days where there was no notes from the previous day.

--- dailynotes.sh
+++ dailynotes.sh
@@ -59,4 +59,20 @@ dailieshelp(){
     echo "completetodo (todo number) - Marks a TODO as complete by prepending an X."
     echo "readnotes - will cat today's daily."
     echo "readtodos - cat today's daily and grep for 'TODO'"
+    echo "copytodos (path to notes file) - read supplied  notes file and copy incomplete TODOs to todays notes file."
+}
+
+copytodos(){
+    if [ -f $1 ]
+        then
+        cat $1 | grep '^TODO' | awk -F': ' '{print $NF}' > ~/dailies/todotemp.txt
+        todocount=$( cat $DAILYNOTEPATH | grep -c '^TODO\|^XTODO' )
+        declare -i num=$todocount
+        while read todo; do
+            echo "TODO${num}: $todo" >> $DAILYNOTEPATH
+            num+=1
+        done<~/dailies/todotemp.txt
+        numcarriedtodos=$( cat $1 | grep -c '^TODO' )
+        echo "Carried over ${numcarriedtodos} TODO(s)"
+    fi
 }
